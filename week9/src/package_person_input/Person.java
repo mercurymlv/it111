bb60392ae23e19ef823e55cd96a8459a05fe2b2b
Matthew Valdez <info@matthewvaldez.com>
2024-05-31 11:41:59 -0700

    Adds Person methods and starts details

--- week9/src/package_person_input/Person.java
+++ week9/src/package_person_input/Person.java
@@ -7,6 +7,8 @@ public class Person {
     char gender;
     int age;
     String zipCode;
+    int cityCode;
+
 
     // now we have our individual violations, as well as the main violations
     boolean violations;
@@ -19,16 +21,74 @@ public class Person {
 
 
     // creating a method named display so that we can print the info in one fell swoop
+    public void display() {
+        System.out.println("This is the information that you have provided:");
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Gender: " + gender);
+        System.out.println("Zip Code: " + zipCode);
+        System.out.println("Your baseline rate is: " + baselineRate + " dollars");
+        System.out.println("Your baseline rate may change based on: age, credit score, violations, and residence");
+        System.out.println("Violations: " + violations);
+        System.out.println("Violation DUI: " + violationDUI);
+        System.out.println("Violation Red Light: " + violationRedLight);
+        System.out.println("Violation Speeding: " + violationSpeeding);
+        System.out.println("Credit Score: " + creditScore);
 
-    public void display(){
-        System.out.println("This is what you have entered!");
-        System.out.println("Name: " +name);
-        System.out.println("Age: " +age);
-        System.out.println("Gender: " +gender);
-        System.out.println("Zip Code: " +zipCode);
-        System.out.println("Your baseline rate is: " +baselineRate+ " dollars");
-        System.out.println("Your baseline rate is calculated without taking the following into consideration: Age, Credit Score, Violations, and Residence");
+    }
 
+    // create a method for violations and credit score
+    // violates OR credit score
+    public double getRate(double monthlyRate) {
+        if (violations == true || creditScore <= 700) {
+            monthlyRate = 300.00;
+        } else {
+            monthlyRate = 0;
+        }
+        return monthlyRate;
+    }
+
+    // new method for age
+    // if you are young or older, you will pay more
+    public double ageCategory(double adjustedRate) {
+        if (age <= 25 || age >= 65) {
+            adjustedRate = 300.00;
+        } else {
+            adjustedRate = 0;
+        }
+        return adjustedRate;
+    }
+
+    // method for state/zip code/strings/changing your string to an integer
+    // in California zip codes are 90-96###
+    // Oregon 97
+    // Washington 98, 99
+    public int identifyRegion(int states) {
+        String zip2;
+        zip2 = zipCode.substring(0, 2);
+        cityCode = Integer.parseInt(zip2);
+
+        if (cityCode >= 90 && cityCode < 97) {
+            System.out.println("You entered " + zipCode + " and you are from California");
+        } else if (cityCode >= 97 && cityCode < 98) {
+            System.out.println("You entered " + zipCode + " and you are from Oregon");
+        } else if (cityCode >= 98 && cityCode < 99) {
+            System.out.println("You entered " + zipCode + " and you are from Washington");
+        } else {
+            System.out.println("You are not from the PNW. Please contact our national office.");
+        }
+        return states;
+    }
+
+    // new method to create a surcharge for California
+    // because of number of cars, pollution, etc..,
+    public double californiaRegion(double surcharge){
+        if (cityCode >= 90 && cityCode < 97){
+            surcharge = 200.00;
+        } else{
+            surcharge = 0;
+        }
+        return surcharge;
     }
 
 }
--- week9/src/package_person_input/PersonDetails.java
+++ week9/src/package_person_input/PersonDetails.java
@@ -9,18 +9,48 @@ public class PersonDetails {
         Person customer;
         customer = new Person();
 
+        // need to enter add'l variables
+        int states = 0;
+        double surchange = 0;
+
+
         // we will be initializing add'l variables right here
         Scanner input = new Scanner(System.in);
         System.out.print("Please enter your name: ");
         customer.name = input.next();
         System.out.print("Please enter your age: ");
         customer.age = input.nextInt();
-        System.out.print("Please enter your gender: ");
+        System.out.print("Please enter your gender by entering F or M or N: ");
         customer.gender = input.next().charAt(0);
-        System.out.print("Please enter your zipcode: ");
+        System.out.print("Please enter your 5-digit zipcode: ");
         customer.zipCode = input.next();
 
+        System.out.print("Any traffic violations? Please enter true for yes and false for no: ");
+        customer.violations = input.nextBoolean();
+
+        // add an if-else to check if and what violations they have
+        if (customer.violations == true){
+            System.out.print("Please type true if you had a DUI, or false if not: ");
+            customer.violationDUI = input.nextBoolean();
+            System.out.print("Please type true if you had a red light violation, or false if not: ");
+            customer.violationRedLight = input.nextBoolean();
+            System.out.print("Please type true if you have received a speeding ticket, or false if not: ");
+            customer.violationSpeeding = input.nextBoolean();
+
+        // not sure if this is necessary, will it just default to false on initialization?
+        } else {
+            customer.violationDUI = false;
+            customer.violationRedLight= false;
+            customer.violationSpeeding = false;
+        }
+
+        System.out.print("Please enter your credit score: ");
+        customer.creditScore = input.nextDouble();
+
         customer.display();
+
+        customer.identifyRegion(states);
+        System.out.println("Your regional surcharge is " +customer.californiaRegion(surchange)+ " dollars");
 
 
     } // end main
